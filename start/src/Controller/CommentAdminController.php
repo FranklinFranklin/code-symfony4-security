bcfd9ffca8c98415bd26cb6e6678965692f75581
franklinfranklin <franklinlivemail@gmail.com>
2022-12-05 18:36:59 +0100

    symfonyChapter4_15:
    - Deny Access in the Controller
    
    this can be done with:
    
    - security.yaml
    - @IsGranted("ROLE_USER") = annotations
    - PHP within the controller = $this->denyAccessUnlessGranted('ROLE_USER');

--- start/src/Controller/CommentAdminController.php
+++ start/src/Controller/CommentAdminController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\CommentRepository;
 use Knp\Component\Pager\PaginatorInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -12,9 +13,15 @@ class CommentAdminController extends Controller
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
+     * @IsGranted("ROLE_USER")
      */
     public function index(CommentRepository $repository, Request $request, PaginatorInterface $paginator)
     {
+//      denyAccessUnlessGranted is the way to grant access to an URL through the controller
+//        $this->denyAccessUnlessGranted('ROLE_USER');
+
+//        * @IsGranted("ROLE_ADMIN") <<< is also a way to grant access
+
         $q = $request->query->get('q');
 
         $queryBuilder = $repository->getWithSearchQueryBuilder($q);
@@ -25,7 +32,7 @@ class CommentAdminController extends Controller
             10/*limit per page*/
         );
 
-        return $this->render('comment_admin/login.twig', [
+        return $this->render('comment_admin/login.html.twig', [
             'pagination' => $pagination,
         ]);
     }
